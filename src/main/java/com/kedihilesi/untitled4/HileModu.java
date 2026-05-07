b6b44196bd83044de4bc381f77bac094eb5489c9
kwoore <kivpatates@gmail.com>
2026-05-07 09:19:18 +0300

    Update HileModu.java

--- src/main/java/com/kedihilesi/untitled4/HileModu.java
+++ src/main/java/com/kedihilesi/untitled4/HileModu.java
@@ -1,1 +1,11 @@
 package com.kedihilesi.untitled4;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Untitled4 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Burayı boş bırakabilirsin, hata vermez.
+        // Ana mod yüklenirken yapılacak bir iş yoksa boş kalması en güvenlisidir.
+    }
+}
